cdcb682292c0e7cf6affa033fbf7ea0519ce7088
Eugene Bogaart <eugene@mongodb.com>
2021-02-08 11:28:20 +0100

    Fixed .location warnings
    Out dynamic IP.

--- azure.tf
+++ azure.tf
@@ -17,7 +17,7 @@
 provider "azurerm" {
     # whilst the `version` attribute is optional,
     # we recommend pinning to a given version of the Provider
-    version = "=2.36"
+    version = "=2.45"
     subscription_id = var.azure_subscription_id
     tenant_id = var.azure_tenant_id
     features {}
@@ -64,7 +64,8 @@ resource "azurerm_private_endpoint" "atlas-group" {
   private_service_connection {
     name                           = mongodbatlas_privatelink_endpoint.test.private_link_service_name
     private_connection_resource_id = mongodbatlas_privatelink_endpoint.test.private_link_service_resource_id
-    is_manual_connection           = true
+
+    is_manual_connection           = true    
     request_message = "Azure Private Link test"
   }
         
@@ -73,7 +74,9 @@ resource "azurerm_private_endpoint" "atlas-group" {
 
 resource "azurerm_public_ip" "demo-vm-ip" {
     name                         = "myPublicIP"
-    location                     = local.location_alt
+    # Looks like changed in azurerm
+    # location                     = local.location_alt
+    location                     = local.location
     resource_group_name          = azurerm_resource_group.atlas-group.name
     allocation_method            = "Dynamic"
 
@@ -84,13 +87,15 @@ resource "azurerm_public_ip" "demo-vm-ip" {
 
 output "public_ip_address" {
   description = "Public IP of azure VM"
-  value       = azurerm_public_ip.demo-vm-ip.*.ip_address
+  value       = azurerm_public_ip.demo-vm-ip.ip_address
 }
 
 # Create Network Security Group and rule
 resource "azurerm_network_security_group" "demo-vm-nsg" {
     name                = "myAtlasDemo"
-    location            = local.location_alt
+    # Looks like changed in azurerm
+    # location                     = local.location_alt
+    location            = local.location
     resource_group_name = azurerm_resource_group.atlas-group.name
 
     # Allow inbound SSH traffic
@@ -139,70 +144,71 @@ resource "azurerm_network_interface_security_group_association" "demo-vm" {
 
 # Create virtual machine
 resource "azurerm_linux_virtual_machine" "demo-vm" {
-    name                  = local.azure_vm_name
-    location              = local.location_alt
-    resource_group_name   = azurerm_resource_group.atlas-group.name
-    size                  = local.azure_vm_size
-    admin_username        = local.admin_username
-    admin_password        = var.admin_password
-    network_interface_ids = [
+   name                  = local.azure_vm_name
+   location              = local.location_alt
+   resource_group_name   = azurerm_resource_group.atlas-group.name
+   size                  = local.azure_vm_size
+   admin_username        = local.admin_username
+   admin_password        = var.admin_password
+   network_interface_ids = [
 	azurerm_network_interface.demo-vm-nic.id,
-    ]
+   ]
 
-    admin_ssh_key {
-        username   = local.admin_username
-        public_key = file(var.public_key_path)
-    }
+   admin_ssh_key {
+       username   = local.admin_username
+       public_key = file(var.public_key_path)
+   }
 
-    os_disk {     
-        caching              = "ReadWrite"
-        storage_account_type = "Standard_LRS"
-    }
+   os_disk {     
+       caching              = "ReadWrite"
+       storage_account_type = "Standard_LRS"
+   }
 
-    source_image_reference {
-        publisher         = "Canonical"
-        offer             = "UbuntuServer"
-        sku               = "18.04-LTS"
-        version           = "latest"
-    }
+   source_image_reference {
+       publisher         = "Canonical"
+       offer             = "UbuntuServer"
+       sku               = "18.04-LTS"
+       version           = "latest"
+   }
 
-    tags = {
-        environment       = "Demo"
-    }
+   tags = {
+       environment       = "Demo"
+   }
 
-    connection {
-        disable_password_authentication = true
-        type 				= "ssh"
-        host 				= self.public_ip_address
-        user 				= self.admin_username
-        password 			= self.admin_password
-        agent 				= true
-        private_key 			= file(var.private_key_path)
-    }
+   connection {
+       disable_password_authentication = true
+       type 				= "ssh"
+       host 				= self.public_ip_address
+       user 				= self.admin_username
+       password 			= self.admin_password
+       agent 				= true
+       private_key 			= file(var.private_key_path)
+   }
 
-    provisioner "remote-exec" {
+   provisioner "remote-exec" {
 
-        connection {
-            host = self.public_ip_address
-            user = self.admin_username
-            password = self.admin_password
-        }
+       connection {
+           host = self.public_ip_address
+           user = self.admin_username
+           password = self.admin_password
+       }
 
-        inline = [
-        "sleep 10",
-        "sudo apt-get -y update",
-        "sudo apt-get -y install python3-pip",
-        "sudo apt-get -y update",
-        "sudo pip3 install pymongo==3.9.0",
-        "sudo pip3 install faker",
-        "sudo pip3 install dnspython",
+       # Below, we unstall some common tools, including mongo client shell
+       inline = [
+       "sleep 10",
+       "sudo apt-get -y update",
+       "sudo apt-get -y install python3-pip",
+       "sudo apt-get -y update",
+       "sudo pip3 install pymongo==3.9.0",
+       "sudo pip3 install faker",
+       "sudo pip3 install dnspython",
 
-        "wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -",
-        "echo 'deb [ arch=amd64 ] http://repo.mongodb.com/apt/ubuntu bionic/mongodb-enterprise/4.4 multiverse' | sudo tee /etc/apt/sources.list.d/mongodb-enterprise.list",
-        "sudo apt-get update",
-	"sudo apt-get install -y mongodb-enterprise-shell"
-        ]
-    }
+       "wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -",
+       "echo 'deb [ arch=amd64 ] http://repo.mongodb.com/apt/ubuntu bionic/mongodb-enterprise/4.4 multiverse' | sudo tee /etc/apt/sources.list.d/mongodb-enterprise.list",
+       "sudo apt-get update",
+	   "sudo apt-get install -y mongodb-enterprise-shell"
+       ]
+   }
 }
 
 
