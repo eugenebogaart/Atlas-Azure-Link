201c3e1e147075b71e0920af6086d9d13813dee8
Eugene Bogaart <eugene@mongodb.com>
2021-02-08 12:01:14 +0100

    Dropped unused var
    Unused attributed removed

--- variables.tf
+++ variables.tf
@@ -13,11 +13,6 @@ variable "azure_tenant_id" {
   type = string
 }
 
-variable "ssh_keys_data" {
-  description = "Public key"
-  type = string
-}
-
 variable "public_key_path" {
   description = "Access path to public key"
   type = string
